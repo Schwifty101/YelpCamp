d139079e61e76215330a7fffe96bf98d32f183d4
Soban Ahmad <sobanahmad@Sobans-MacBook-Pro.local>
2024-08-07 16:13:05 +0500

    Update files and add .gitignore

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const multer = require('multer');
+const { storage } = require('../cloudinary')
+const upload = multer({ storage });
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { validateCampground, isLoggedIn, isAuthor } = require('../middleware');
@@ -9,7 +12,14 @@ router.get('/', catchAsync(campgrounds.index));
 
 router.get('/new', isLoggedIn, campgrounds.renderCampgroundForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground))
+router.post('/', upload.array('image'), (req, res) => {
+    // Send the uploaded file and form data back in the response
+    // res.send({ body: req.body, files: req.files });
+    console.log(req.body, req.files);
+    res.send('IT WORKED');
+});
+
 
 router.get('/:id', catchAsync(campgrounds.renderCampground));
 
